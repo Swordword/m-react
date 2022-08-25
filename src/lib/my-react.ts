d5f455269dc1ff9ee438b835d79a0b3e38afbcd9
张建宇 <massivejohn123@gmail.com>
2022-08-25 16:55:06 +0800

    my-react

--- src/lib/my-react.ts
+++ src/lib/my-react.ts
@@ -1,4 +1,65 @@
-const a = 111222
-export default {
-  a
+function createTextElement(text: string) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
 }
+
+function createElement(
+  type: string,
+  props?: Record<string, any> | null,
+  ...children: Array<object | string>
+) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === 'object' ? child : createTextElement(child)
+      ),
+    },
+  }
+}
+
+function render(element: any, container: HTMLElement) {
+  const dom =
+    element.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(element.type)
+  const isProperty = (key: string) => key !== 'children'
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name]
+    })
+
+  element.props.children.forEach((child: any) => {
+    render(child, dom)
+  })
+  container.appendChild(dom)
+}
+
+let nextUnitOfWork:any = null
+function workLoop(deadline){
+  let shouldYield = false
+  while(nextUnitOfWork &&! shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(nextUnitOfWork: any) {
+  
+}
+
+
+// const Didact = {
+//   createElement,
+// }
+
+export { createElement, render }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,37 @@
-import { a } from './lib/my-react.ts'
+import * as Didact from './lib/my-react'
 function main() {
-  console.log('111222')
+  /**
+   * const element = <h1 title='foo'>Hello</h1>
+   * const container = document.getElementById('root')
+   * ReactDOM.render(element, container)
+   */
+
+  const container = document.getElementById('root') as HTMLElement
+
+  // const element = React.createElement('h1',{title: 'foo'}, 'Hello')
+
+  // const element = {
+  //   type: 'h1',
+  //   props: {
+  //     title: 'foo',
+  //     children: 'Hello',
+  //   },
+  // }
+
+  // const node = document.createElement(element.type)
+  // node['title'] = element.props.title
+  // const text = document.createTextNode('')
+  // text.nodeValue = element.props.children
+  // node.appendChild(text)
+  // container.appendChild(node)
+
+  const element = Didact.createElement(
+    'div',
+    { id: 'foo' },
+    Didact.createElement('a', null, 'bar'),
+    Didact.createElement('b')
+  )
+  Didact.render(element, container)
 }
 
 main()
